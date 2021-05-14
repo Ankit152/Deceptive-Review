e07102732dd382edacf731f321354d69f30e9ff3
Ankit Kurmi <ankitkurmi152@gmail.com>
2021-05-14 23:38:09 +0530

    added model.py

---
+++ model.py
@@ -0,0 +1,61 @@
+# importing the libraries
+import numpy as np
+import pandas as pd
+import string
+import re
+import nltk
+import seaborn as sns
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.utils import to_categorical
+from keras.layers import Dense, Embedding, LSTM, Bidirectional, Dropout
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report
+
+print("All the modules imported....")
+
+data = pd.read_csv('deceptive-opinion.csv')
+print("Dataset loaded....")
+
+
+# preprocessing the data
+def cleaning(text):
+    text = text.lower()
+    pattern = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    clean = re.compile('<.*?>')
+    text = re.sub(clean,'',text)
+    text = pattern.sub('', text)
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"that's", "that is", text)        
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text) 
+    text = re.sub(r"\'ll", " will", text)  
+    text = re.sub(r"\'ve", " have", text)  
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"don't", "do not", text)
+    text = re.sub(r"did't", "did not", text)
+    text = re.sub(r"can't", "can not", text)
+    text = re.sub(r"it's", "it is", text)
+    text = re.sub(r"couldn't", "could not", text)
+    text = re.sub(r"have't", "have not", text)
+
+    text = re.sub(r"[,.\"!@#$%^&*(){}?/;`~:<>+=-]", "", text)
+    tokens = word_tokenize(text)
+    table = str.maketrans('', '', string.punctuation)
+    stripped = [w.translate(table) for w in tokens]
+    words = [word for word in stripped if word.isalpha()]
+    text = ' '.join(words)
+    return text
+
+print("Cleaning is taking place....")
+data['text'] = data['text'].map(cleaning)
+print("Cleaning of the dataset is done....")
